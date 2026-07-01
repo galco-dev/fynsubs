6788b3a062a280d5751fd7643d6f75b6ba4a19a3
galco-dev <galco2026@gmail.com>
2026-07-02 03:23:04 +0400

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -18,7 +18,7 @@ export default function Home() {
       <HeroSection />
       <FeaturesSection />
       <HowItWorksSection />
-      <InfrastructureSection />
+      
       <MetricsSection />
       <IntegrationsSection />
       <SecuritySection />
